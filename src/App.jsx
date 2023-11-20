950c7a250f27427239f17c0e6ef321f539fc42a7
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-11-20 15:16:49 -0500

    user can now see numerical x position of mouse

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,26 @@ import { useState, useEffect } from "react"
 
 function App() {
 
+  const [mouseXposition, setMouseXPosition] = useState({x:0});
+
 
 
   useEffect(() => {
-    const changeNumber = () => {
-      // logic
+    const updateMouseXPosition = (event) => {
+      setMouseXPosition({x: event.clientX})
     }
-    window.addEventListener('pointermove',changeNumber)
-  })
+
+    window.addEventListener('pointermove',updateMouseXPosition)
+
+    return () => {
+      window.removeEventListener('pointermove', updateMouseXPosition)
+    }
+  }, [])
 
   return (
     <>
-      <h1> The width of my window is {window.innerWidth} px </h1>
-      <h1>0</h1>
+    {/*Math.truc() used to remove float numbers*/}
+      <h1>{Math.trunc(mouseXposition.x)}</h1>
     </>
   )
 }
