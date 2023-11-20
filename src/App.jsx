fa658e6c2ea316b25b2b94a68347277ea1ecaf24
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-11-19 22:04:07 -0500

    pseudocode for pointer move event listener

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-
+import { useState, useEffect } from "react"
 
 function App() {
 
 
+
+  useEffect(() => {
+    const changeNumber = () => {
+      // logic
+    }
+    window.addEventListener('pointermove',changeNumber)
+  })
+
   return (
     <>
-      <h1></h1>
+      <h1> The width of my window is {window.innerWidth} px </h1>
+      <h1>0</h1>
     </>
   )
 }
